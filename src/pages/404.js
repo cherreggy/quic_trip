908517dc531d4dce34b95544de8bc2fb4a0e2b13
cherreggy <xuanxuanniaoniao@163.com>
2023-04-11 21:48:19 +0800

    End with status codes.

---
+++ src/pages/404.js
@@ -0,0 +1,14 @@
+export default function Custom404() {
+  return (
+    <div
+      style={{
+        display: "flex",
+        paddingTop: "10rem",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <img src="404.png" style={{ width: "50rem" }} />
+    </div>
+  );
+}
